e488e913010d4b0b5607eaeeec366b9d26d1fddd
Hữu An <huuan152@gmail.com>
2021-11-08 16:58:51 +0700

    admin

--- core/settings.py
+++ core/settings.py
@@ -82,9 +82,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bookshop',
         'USER': 'root',
-        'PASSWORD': '13041807',
-        'HOST': 'localhost',
-        'PORT': '3307',
+        'PASSWORD': 'HuuAn1522001@',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
